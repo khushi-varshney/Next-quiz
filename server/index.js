b161ad03a2179b4b8fe6d017d5311c1424a9dc54
khushi varshney <anuragkhushi703@gmail.com>
2024-11-16 11:35:11 +0530

    add

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv"; 
-import bcrypt from "bcrypt";
 
 const app = express();
 app.use(express.json());
