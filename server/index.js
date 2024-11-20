b70b35d056565b7ba3be0da35108996ba03ab0f0
khushi varshney <anuragkhushi703@gmail.com>
2024-11-20 22:45:23 +0530

    add

--- server/index.js
+++ server/index.js
@@ -7,20 +7,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-// app.use(cors());
-// app.use(cors({
-//   origin: ['https://next-quiz-bice.vercel.app/'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }))
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(cors({
+  origin: ['https://next-quiz-bice.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}))
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
 
 dotenv.config();
 const DB=process.env.MONGODB_URI;
