8426665b12ee0ce2eab35a972e688fd768c508ec
khushi varshney <anuragkhushi703@gmail.com>
2024-11-21 16:13:51 +0530

    add

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ app.post("/login", async(req, res) => {
   });
 });
 
-app.post("/signup", async(req, res) => {
+app.post("/sign", async(req, res) => {
   const { name, email, password } = req.body;
   User.findOne({ email }).then((user) => {
     if (user) {
