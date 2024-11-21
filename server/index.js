413e8d18a26dde6aedf0164b7ed5e88d4289c3a8
khushi varshney <anuragkhushi703@gmail.com>
2024-11-21 15:57:47 +0530

    add

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, application/json, authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 })
 
