cfbffe594c85ec5dd734149d8787b9b445e12b6d
khushi varshney <anuragkhushi703@gmail.com>
2024-11-22 21:20:55 +0530

    statsu

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
