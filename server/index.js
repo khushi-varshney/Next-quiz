9df107eb9f2ec6a81dfe492d560ed4e176cde4ae
khushi varshney <anuragkhushi703@gmail.com>
2024-11-22 21:00:50 +0530

    add

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 })
