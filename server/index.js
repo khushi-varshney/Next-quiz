c8b993f853544350f9c784f4ebc91205fb688893
khushi varshney <anuragkhushi703@gmail.com>
2024-11-22 21:13:49 +0530

    add

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-// app.use(cors())
+app.use(cors())
 // app.use(cors({
 //   origin: ['https://next-quiz-bice.vercel.app/'],
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
