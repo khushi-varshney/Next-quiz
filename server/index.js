10f57e28006a1a4722eb3bb5acdc4878adfd5444
khushi varshney <anuragkhushi703@gmail.com>
2024-11-23 15:19:12 +0530

    add

--- server/index.js
+++ server/index.js
@@ -8,13 +8,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use(cors({
+  origin: '*',
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*, https://next-quiz-bice.vercel.app/");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization");
+app.options('*', cors())
+
+
+app.all('', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  //Auth Each API Request created by user.
   next();
-})
+});
+
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*, https://next-quiz-bice.vercel.app/");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization");
+//   next();
+// })
 
 dotenv.config();
 const DB="mongodb+srv://user:koHKbEuxFTqC6OSk@cluster0.4lsix.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0";
