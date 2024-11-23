5b300bb5118309f4a803070a989d237d8bcf3b99
khushi varshney <anuragkhushi703@gmail.com>
2024-11-23 14:43:57 +0530

    add

--- server/index.js
+++ server/index.js
@@ -8,15 +8,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use(cors({
-  origin: "https://next-quiz-bice.vercel.app/", // Vervang door de juiste oorsprong
-  methods: "GET, POST, PUT, DELETE", // Vervang door de toegestane methoden
-  allowedHeaders: "Content-Type", // Vervang door de toegestane headers
-  credentials: true, // Schakel cookies en verificatie in indien nodig
-  optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//   origin: "https://next-quiz-bice.vercel.app/", // Vervang door de juiste oorsprong
+//   methods: "GET, POST, PUT, DELETE", // Vervang door de toegestane methoden
+//   allowedHeaders: "Content-Type", // Vervang door de toegestane headers
+//   credentials: true, // Schakel cookies en verificatie in indien nodig
+//   optionsSuccessStatus: 200
+// }));
 
+// app.use(cors())
+// app.use(cors({
+//   origin: ['https://next-quiz-bice.vercel.app/'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: false
+// }))
 
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// })
 
 dotenv.config();
 const DB="mongodb+srv://user:koHKbEuxFTqC6OSk@cluster0.4lsix.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0";
