5fa6bb6b77d64e13ae0013f162416ffc0d8a4e64
khushi varshney <anuragkhushi703@gmail.com>
2024-11-23 14:59:57 +0530

    add

--- server/index.js
+++ server/index.js
@@ -1,28 +1,13 @@
 import express from "express";
-import cors from "cors";
+
 import mongoose from "mongoose";
 import * as dotenv from "dotenv"; 
-import bcrypt from "bcrypt"
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-// app.use(cors({
-//   origin: "https://next-quiz-bice.vercel.app/", // Vervang door de juiste oorsprong
-//   methods: "GET, POST, PUT, DELETE", // Vervang door de toegestane methoden
-//   allowedHeaders: "Content-Type", // Vervang door de toegestane headers
-//   credentials: true, // Schakel cookies en verificatie in indien nodig
-//   optionsSuccessStatus: 200
-// }));
-
-// app.use(cors())
-// app.use(cors({
-//   origin: ['https://next-quiz-bice.vercel.app/'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: false
-// }))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
