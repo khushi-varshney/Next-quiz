8a23d79d11ca14f8a6307f082ae8e496d49504e5
khushi varshney <anuragkhushi703@gmail.com>
2024-11-26 21:50:00 +0530

    add

--- server/index.js
+++ server/index.js
@@ -23,10 +23,15 @@ app.use(express.urlencoded());
 // });
 
 
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization");
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   next();
 })
 
