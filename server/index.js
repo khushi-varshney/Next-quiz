9a07ba956bffedd2e004cb80093e51735df4ba06
khushi varshney <anuragkhushi703@gmail.com>
2024-11-26 21:33:28 +0530

    add

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.all('', function(req, res, next) {
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*, https://next-quiz-bice.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization");
   next();
