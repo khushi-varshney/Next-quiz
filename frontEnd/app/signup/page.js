7c64e650bf73059e54d6a192a029fb674067b689
khushi varshney <anuragkhushi703@gmail.com>
2024-12-22 16:22:35 +0530

    add

--- frontEnd/app/signup/page.js
+++ frontEnd/app/signup/page.js
@@ -68,7 +68,7 @@ const page = ({setLoginRegister}) => {
   }
 
   return (
-    <div className='overflow-x-hidden'>
+    <div className='overflow-hidden'>
       <div className="bg-blue-200 overflow-y-hidden flex justify-center items-center">
       <ToastContainer
         position="top-right"
